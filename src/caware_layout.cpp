dd9ac266f63cdb4f65d57e3476fa14145b72b502
Sriram P C <sriram.pc@gmail.com>
2014-09-24 17:02:43 -0400

    Move the cache aware layout to a helper class

--- src/caware_layout.cpp
+++ src/caware_layout.cpp
@@ -8,9 +8,6 @@
 typedef std::vector<uint64_t>::size_type size_type;
 typedef std::vector<size_type> size_type_vector;
 
-size_type_vector gCompleteLevelSize;
-size_type_vector gCompleteTreeSize;
-
 template<typename T>
 void write_stvector(std::vector<T> sts, std::ostream& os) {
     std::ostream_iterator<T> outs(os, " ");
@@ -18,57 +15,6 @@ void write_stvector(std::vector<T> sts, std::ostream& os) {
     os << std::endl;
 }
 
-
-void init_global(size_type m, size_type l){
-    gCompleteLevelSize.resize(l);
-    gCompleteTreeSize.resize(l);
-    gCompleteTreeSize[0] =  gCompleteLevelSize[0] = m;
-    for(auto i = 1u; i < l; i++){
-        gCompleteLevelSize[i] = gCompleteLevelSize[i - 1] * (m + 1);
-        gCompleteTreeSize[i] = gCompleteTreeSize[i - 1] + gCompleteLevelSize[i];
-    }
-}
-
-void subtree_size(const size_type& d, const size_type& k, const size_type& m,
-                  size_type_vector& stsize){
-    size_type_vector ls;
-    stsize.resize(m + 1);
-    ls.resize(m + 1);
-    // No. of elements at last level.
-    auto dk = d - gCompleteTreeSize[k - 2];
-    //
-    auto q = dk / gCompleteLevelSize[k - 2];
-    auto r = dk % gCompleteLevelSize[k - 2];
-#ifdef NDEBUG
-    std::cout << "dk : " << dk
-              << " q : " << q
-              << " r : " << r
-              << std::endl;
-#endif
-    //TODO: cache
-    for(auto j = 0u; j <= m; j++)
-        if(j < q)
-            ls[j] = gCompleteLevelSize[k - 2];
-        else if(j > q)
-            ls[j] = 0;
-        else
-            ls[j] = r;
-
-#ifdef NDEBUG
-    std::cout << "LS : ";
-    write_stvector(ls, std::cout);
-#endif
-    //TODO: cache st size
-    for(auto j = 0u; j <= m; j++)
-        if(k > 2)
-            stsize[j] = ls[j] + gCompleteTreeSize[k - 3];
-        else
-            stsize[j] = ls[j];
-#ifdef NDEBUG
-    std::cout << "ST : ";
-    write_stvector(stsize, std::cout);
-#endif
-}
 
 size_type int_log(const size_type& b,
                   const size_type& x){
@@ -80,6 +26,95 @@ size_type int_log(const size_type& b,
     }
     return vl;
 }
+
+class CacheAwareLayoutHelper{
+   private:
+    size_type_vector completeLevelSize;
+    size_type_vector completeTreeSize;
+    size_type d_cache = 0;
+    size_type k_cache = 0;
+    size_type m_cache = 0;
+    size_type_vector stsize;
+
+    void init_sizes(const size_type &m,
+                    const size_type &l){
+        completeLevelSize.resize(l);
+        completeTreeSize.resize(l);
+        completeTreeSize[0] =  completeLevelSize[0] = m;
+        for(auto i = 1u; i < l; i++){
+            completeLevelSize[i] = completeLevelSize[i - 1] * (m + 1);
+            completeTreeSize[i] = completeTreeSize[i - 1] + completeLevelSize[i];
+        }
+#ifdef NDEBUG
+    write_stvector(completeLevelSize, std::cout);
+    write_stvector(completeTreeSize, std::cout);
+#endif
+    }
+
+
+  public:
+    CacheAwareLayoutHelper(const size_type& n,
+                           const size_type& l,
+                           const size_type& m):d_cache(n),k_cache(l),m_cache(m){
+        init_sizes(m,l);
+        subtree_size(n, l, m);
+    }
+
+    size_type_vector& subtree_size(const size_type& d, const size_type& k,
+                                   const size_type& m){
+        size_type_vector ls;
+        stsize.resize(m + 1);
+        ls.resize(m + 1);
+        // No. of elements at last level.
+        auto dk = d - completeTreeSize[k - 2];
+        //
+        auto q = dk / completeLevelSize[k - 2];
+        auto r = dk % completeLevelSize[k - 2];
+#ifdef NDEBUG
+        std::cout << "dk : " << dk
+                  << " q : " << q
+                  << " r : " << r
+                  << std::endl;
+#endif
+        //TODO: cache
+        for(auto j = 0u; j <= m; j++)
+            if(j < q)
+                ls[j] = completeLevelSize[k - 2];
+            else if(j > q)
+                ls[j] = 0;
+            else
+                ls[j] = r;
+
+#ifdef NDEBUG
+        std::cout << "LS : ";
+        write_stvector(ls, std::cout);
+#endif
+        //TODO: cache st size
+        for(auto j = 0u; j <= m; j++)
+            if(k > 2)
+                stsize[j] = ls[j] + completeTreeSize[k - 3];
+            else
+                stsize[j] = ls[j];
+#ifdef NDEBUG
+        std::cout << "ST : ";
+        write_stvector(stsize, std::cout);
+#endif
+        return stsize;
+    }
+
+    size_type_vector& subtree_size_cache(const size_type& d,
+            const size_type& k,
+            const size_type& m){
+        if(d_cache == d && m_cache == m && k_cache == k)
+            return stsize;
+        // initialization
+        d_cache = d;
+        k_cache = k;
+        m_cache = m;
+        return subtree_size(d,k,m);
+    }
+};
+
 
 template <typename T>
 void caware_layout(std::vector<T>& srtx,
@@ -95,14 +130,8 @@ void caware_layout(std::vector<T>& srtx,
     std::cout << "Levels : " << l << std::endl;
 #endif
 
-    init_global(m, l);
-#ifdef NDEBUG
-    write_stvector(gCompleteLevelSize, std::cout);
-    write_stvector(gCompleteTreeSize, std::cout);
-#endif
+    CacheAwareLayoutHelper chelper(n, l, m);
     size_type i = 0, l_ptr = 0, c_ptr = 0;
-    size_type_vector stsize;
-    stsize.resize(m + 1);
 
     // Root node indicies and level
     ctree[0] = 0;
@@ -126,7 +155,9 @@ void caware_layout(std::vector<T>& srtx,
             for(auto j = 0u; j < m; j++)
                 ctree[c_ptr + j] = srtx[x++];
         } else {
-            subtree_size(d, k, m, stsize);
+            const size_type_vector& stsize =
+                chelper.subtree_size_cache(d,k,m);
+
             y = x;
             // Update entries for current node
             for(auto j = 0u; j < m; j++) {
@@ -146,21 +177,16 @@ void caware_layout(std::vector<T>& srtx,
                 }
             }
         }
-        // move to the next
+        // move current forward
         i += m;
         c_ptr += m;
     }
 }
 
 int main(int argc, char* argv[]){
-    // test!
 
-    //init_global(m, l);
-    //subtree_size(d, k, m, stsize);
-    //write_stvector(stsize, std::cout);
-
-    size_type m = 4, beg = m * 8,
-        end = 1 + (m * 21);
+    size_type m = 3, beg = m * 8,
+        end = 1 + (m * 40);
     for(size_type i = beg; i  < end; i += m) {
         size_type_vector stsize;
         size_type n = i;
@@ -175,13 +201,11 @@ int main(int argc, char* argv[]){
         caware_layout(tst, m, ct);
         write_stvector(ct, std::cout);
 
-        std::cout << "Search : "
-                  << pure_c::implicit_heap_search(ct.begin(), ct.end(), m + 1, n)
-                  << " "
-                  << pure_c::implicit_heap_search(ct.begin(), ct.end(), m + 1, -1)
-                  << " "
-                  << pure_c::implicit_heap_search(ct.begin(), ct.end(), m + 1, n/2)
-                  << std::endl;
+        std::cout << "Search : ";
+        for(auto j = -1; j <= (int) (i+2);j++) {
+            std::cout << pure_c::implicit_heap_search(ct.begin(), ct.end(), m + 1, j) << " ";
+        }
+        std::cout << std::endl;
     }
     return 0;
 }
